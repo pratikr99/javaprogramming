2fe7d27d4020a85e546a7a5b9d2c4d093068373d
pratikr99 <pratikraghuvaran@gmail.com>
2026-01-18 19:55:50 -0800

    Added comment

--- module-01-helloworld/src/main/java/com/course/HelloWorld.java
+++ module-01-helloworld/src/main/java/com/course/HelloWorld.java
@@ -1,6 +1,12 @@
 package com.course;
+/*
+* This is the Hello World program in Java. It does not take any inputs, it just
+* prints the message Hello World!.
+* You compile using javac com.course.HelloWorld and run java com.course.JavaWorld
+*/
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
+
